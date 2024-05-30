deb3a8f9e838f8a33ceeb03856be3acfc121f5d9
King <2175616761@qq.com>
2024-05-30 23:31:51 +0800

    有苹果，有蛇,enough

---
+++ Snake/src/fibonacci_lfsr.v
@@ -0,0 +1,45 @@
+/**
+ * @copyright ==================================================================
+ *  Copyright (c) 2024-05-30.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the
+ *  distribution.
+ *  3. All advertising materials mentioning features or use of this software
+ *  must display the following acknowledgement:
+ *  This product includes software developed by the SJ Group. and
+ *  its contributors.
+ *  4. Neither the name of the Group nor the names of its contributors may
+ *  be used to endorse or promote products derived from this software
+ *  without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY SongJian, GROUP AND CONTRIBUTORS
+ *  ===================================================================
+ * @file fibonacci_lfsr.v
+ * @author KSJ
+ * @date 2024-05-30
+ * @version
+ * @brief 伪随机数生成器
+*/
+module fibonacci_lfsr(clk, reset, random_out);
+  parameter WIDTH = 32;
+  input clk, reset;
+  output reg [WIDTH-1:0] random_out;
+  wire feedback;
+  assign feedback = random_out[WIDTH-1] ^ random_out[14] ^ random_out[13] ^ random_out[11];
+
+  always @(posedge clk or negedge reset) begin
+    if (~reset)
+      random_out <= {WIDTH{1'b1}};
+    else
+      random_out <= {random_out[WIDTH-2:0], feedback};
+  end
+endmodule
--- Snake/src/snake_model.v
+++ Snake/src/snake_model.v
@@ -2,7 +2,7 @@ module snake_model(
   clk,reset,left,right,up,down,pix_x,pix_y,
   color
 );
-parameter speed = 1,
+parameter speed = 5,
   width = 640,
   height = 480,
   MAX_LEN = 100,
@@ -21,8 +21,8 @@ input signed [31:0] pix_y;
 output reg [11:0]color;
 
 reg signed [15:0] dir[1:0];  // 方向 {0,1},...,
-reg [31:0] food_x;
-reg [31:0] food_y;
+reg signed [31:0] food_x;
+reg signed [31:0] food_y;
 //-------------------------------------------
 reg signed [31:0] snake_pos[MAX_LEN:0][1:0]; // 0:x, 1:y
 reg [31:0] snake_len = 1;
@@ -34,7 +34,7 @@ always @(posedge clk or negedge reset) begin
     clk_10Hz<=0;
     clk_counter<=0;
   end else begin
-    if (clk_counter == 100000000/2-1) begin  // TODO(SJ) change to 10000000/2-1
+    if (clk_counter == 10000000/2-1) begin
       clk_counter<=0;
       clk_10Hz<=~clk_10Hz;
     end else clk_counter <= clk_counter+1;
@@ -58,7 +58,6 @@ always @(posedge clk or negedge reset) begin
     end
   end
 end
-
 function in_circle;
   input signed [31:0] s_x;
   input signed [31:0] s_y;
@@ -82,27 +81,52 @@ function in_snake;
     end
   end
 endfunction
+//--------------------------random-----------------------------------------
+wire signed [31:0] rand;
+fibonacci_lfsr lfsr(.clk(clk), .reset(reset), .random_out(rand));
+function signed[31:0] next_rand;
+  input signed[31:0] rand;
+  begin
+    next_rand = {rand[30:0], rand[31] ^ rand[2]};
+  end
+endfunction
 //--------------------------move-------------------------------------
-// always @(posedge clk_10Hz) begin:move
-//   integer k;
-//   if (!reset) begin
-//     snake_pos[0][0] <= width/2;
-//     snake_pos[0][1] <= height/2;
-//   end else begin
-//     for (k = snake_len-1-1;k >= 0;k=k-1)begin
-//       snake_pos[k][0] <= snake_pos[k+1][0];
-//       snake_pos[k][1] <= snake_pos[k+1][1];
-//     end
-//     snake_pos[snake_len-1][0] <= (snake_pos[snake_len-1][0] + speed*dir[0] + width) % width;
-//     snake_pos[snake_len-1][1] <= (snake_pos[snake_len-1][1] + speed*dir[1] + height) % height;
-//   end
-// end
+wire eaten;
+assign eaten = in_circle(snake_pos[snake_len-1][0],snake_pos[snake_len-1][1],food_x,food_y);
+always @(posedge clk_10Hz) begin:move
+  integer k;
+  if (!reset) begin
+    snake_pos[0][0] <= width/2;
+    snake_pos[0][1] <= height/2;
+    food_x <= {rand}%width;
+    food_y <= {next_rand(rand)}%height;
+  end
+  // else if(eaten) begin
+  //   // 吃到食物了
+  //   food_x = {rand}%width;
+  //   food_y = {next_rand(rand)}%height;
+  //   snake_len = snake_len + 1;
+  //   snake_pos[snake_len-1][0] = (snake_pos[snake_len-1][0] + speed*dir[0] + width) % width;
+  //   snake_pos[snake_len-1][1] = (snake_pos[snake_len-1][1] + speed*dir[1] + height) % height;
+  // end
+  else begin
+    for (k = snake_len-1-1;k >= 0;k=k-1)begin
+      snake_pos[k][0] <= snake_pos[k+1][0];
+      snake_pos[k][1] <= snake_pos[k+1][1];
+    end
+    snake_pos[snake_len-1][0] <= (snake_pos[snake_len-1][0] + speed*dir[0] + width) % width;
+    snake_pos[snake_len-1][1] <= (snake_pos[snake_len-1][1] + speed*dir[1] + height) % height;
+  end
+end
+
 //--------------------------show----------------------------------------
 always @(posedge clk) begin
-  if (pix_x*pix_x + pix_y*pix_y < 1000) begin
-    color = 12'h0ff;
+  if (in_snake(pix_x,pix_y)) begin
+    color <= 12'h0ff;
+  end else if(in_circle(pix_x,pix_y,food_x,food_y)) begin
+    color <= 12'hf00;
   end else begin
-    color = 12'hfff;
+    color <= 12'hfff;
   end
 end
 endmodule
