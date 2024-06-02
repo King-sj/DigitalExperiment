a79e6b4337f3eeab821a1defe401e81f85bbc40d
King <2175616761@qq.com>
2024-06-03 00:50:03 +0800

    ✨ feat(keybaord): 添加键盘驱动，但是貌似不灵敏

--- Snake/src/snake.v
+++ Snake/src/snake.v
@@ -24,7 +24,6 @@
  *  THIS SOFTWARE IS PROVIDED BY SongJian, GROUP AND CONTRIBUTORS
  *  ===================================================================
  * @file snake.v
- * @author KSJ
  * @date 2024-05-30
  * @version 0.0.1
  * @brief 贪吃蛇
@@ -44,7 +43,9 @@ module snake (
   output [3:0] left_wei,  // 左边的数码管
   output [7:0] left_duan,  //
   output [3:0] right_wei,  //
-  output [7:0] right_duan
+  output [7:0] right_duan,
+  inout ps2_clk,
+  inout ps2_data
 );
 wire [11:0]color;
 wire [9:0]x,y;  // 640x480
@@ -74,7 +75,7 @@ wire [9:0]x,y;  // 640x480
     .noisy_signal(down),
     .clean_signal(down_clean)
   );
-//-------------------------------------------------------------------------
+//-----------------------------VGA--------------------------------------------
 vga vga_shower(
   .clk(clk),
   .new_data(color),
@@ -86,13 +87,21 @@ vga vga_shower(
   .pos_x(x),
   .pos_y(y)
 );
-
+//------------------keyboard-------------------
+wire valid;
+wire [7:0] ascii_code;
+ps2 ps(clk, reset,ps2_clk,ps2_data,valid,ascii_code);
+wire right_p,left_p,up_p,down_p;
+assign right_p = right_clean || ascii_code == 8'h44;
+assign left_p = left_clean || ascii_code == 8'h41;
+assign up_p = up_clean || ascii_code == 8'h57;
+assign down_p = down_clean || ascii_code == 8'h53;
 //---------------------------------------------------
 wire [15:0] score;
 wire game_over;
 snake_model snaker(
   .clk(clk),.reset(reset),
-  .left(left_clean),.right(right_clean),.up(up_clean),.down(down_clean),
+  .left(left_p),.right(right_p),.up(up_p),.down(down_p),
   .pix_x(x),.pix_y(y),
   .color(color),.score(score),.game_over(game_over)
 );
